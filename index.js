72f10fc3f7e4d663970117fe5da6393df524db21
Timothy Downing <downingkeys@gmail.com>
2022-05-14 07:56:38 -0700

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ function Phrase(content) {
 
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+    return this.letters().toLowerCase();
+  }
+
+  // Returns the letters in the content.
+  // For example:
+  //   new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    return (this.content.match(/[a-z]/gi) || []).join("");
   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
--- test/test.js
+++ test/test.js
@@ -14,5 +14,22 @@ describe("Phrase", function() {
       let plainPalindrome = new Phrase("racecar");
       assert(plainPalindrome.palindrome());
     });
+
+    it("should return true for a mixed-case palindrome", function() {
+      let mixedCase = new Phrase("RaceCar");
+      assert(mixedCase.palindrome());
+    });
+
+    it("should return true for a palindrome with punctuation", function() {
+      let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+      assert(punctuatedPalindrome.palindrome());
+    });
+  });
+
+  describe("#letters", function() {
+    it("should return only letters", function() {
+      let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+      assert.strictEqual(punctuatedPalindrome.letters(), "MadamImAdam");
+    });
   });
 });
