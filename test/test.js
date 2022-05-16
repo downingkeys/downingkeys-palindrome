caa8d3418d848a373865e8bfcd4ef5eba08b5f0a
Timothy Downing <downingkeys@gmail.com>
2022-05-16 16:42:54 -0700

    Bump version here too

--- test/test.js
+++ test/test.js
@@ -24,6 +24,11 @@ describe("Phrase", function() {
       let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
       assert(punctuatedPalindrome.palindrome());
     });
+
+    it("should return false for an empty string", function() {
+      let emptyPhrase = new Phrase("");
+      assert(!emptyPhrase.palindrome());
+    });
   });
 
   describe("#letters", function() {
